674461995e0f09d9a6814c2cdfa2f45aaf91eece
not a duck <kaitlynewing@gmail.com>
2018-10-14 19:59:18 -0400

    pawn diagonal move

--- src/ca/humber/chess/Pawn.java
+++ src/ca/humber/chess/Pawn.java
@@ -45,33 +45,48 @@ public class Pawn extends ChessPiece
 //        }
 //        else return false;
 //        
+        int differenceR = r2 - r1;
+        int differenceC = c2 - c1;
         
         //stay in same column
         if( c1 == c2)
         {
-           int difference = r2 - r1;
+           
             //if first turn move 1 or 2
             if(firstMove)//if first turn move 1 or 2
             {
-                if (Math.abs(difference) > 2) return false;
+                if (Math.abs(differenceR) > 2) return false;
             }
             //otherwise can only move 1 space
-            else if (Math.abs(difference) > 1) return false;
+            else if (Math.abs(differenceR) > 1) return false;
             
             
             //check if enemy piece is in the way
             if(board[r2][c2] != null) return false; 
             
-            //if its white can move up 
-            if(colour.equals("white") && difference < 0) return false;
-            //if its black moves down
-            else if (colour.equals("black") && difference > 0) return false;
+            if (!checkDirection(differenceR)) return false;
         }
         //else if enemy piece is diagonally in front of the pawn
-        
+        else if (Math.abs(differenceR) == 1 && Math.abs(differenceC) == 1) //diagonal move requested
+        {
+            if(board[r2][c2] != null && !board[r2][c2].colour.equals(colour))//if enemy piece is there
+            {
+                if (!checkDirection(differenceR)) return false;
+            }
+            else return false; //enemy piece not there
+        }
         else return false;//if not in same column return false
         
-        //firstMove = false;
+        firstMove = false;
+        return true;
+    }
+    
+    public boolean checkDirection(int differenceR)
+    {
+        //if its white can move up 
+                if(colour.equals("white") && differenceR < 0) return false;
+                //if its black moves down
+                else if (colour.equals("black") && differenceR > 0) return false;
         return true;
     }
 }
