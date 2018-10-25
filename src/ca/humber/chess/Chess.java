b69a02e3d9b70faacc3c5b9038e28fa5126d569d
not a duck <kaitlynewing@gmail.com>
2018-10-24 20:25:34 -0400

    instructions

--- src/ca/humber/chess/Chess.java
+++ src/ca/humber/chess/Chess.java
@@ -9,8 +9,9 @@ public class Chess
     public static void main(String[] args) 
     {
         GameManager game = new GameManager();
-        
-        System.out.print("Enter the movement like: 'A 2 A 3' \n \n");
+        System.out.println("Instructions:");
+        System.out.println("Enter the movement with the starting point first and the destination second");
+        System.out.println("for example: 'A 2 A 3'");
         
         game.Start();
         game.Run();
